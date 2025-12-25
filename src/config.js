331a8eef4118fd742b4689554c6f587e1f7ea9e8
vrush2000 <syamsul8895@gmail.com>
2025-12-25 17:27:11 +0700

    Fix missing config.js and restore server functionality

---
+++ src/config.js
@@ -0,0 +1,19 @@
+
+/**
+ * External API Configurations
+ */
+export const API_CONFIG = {
+  SHOLAT: {
+    MYQURAN: 'https://api.myquran.com/v2/sholat',
+    NOMINATIM: 'https://nominatim.openstreetmap.org/reverse'
+  },
+  HADITS: {
+    GADING: 'https://api.hadith.gading.dev'
+  },
+  KEMENAG: {
+    HARI_LIBUR: 'https://api-harilibur.vercel.app/api',
+    PESANTREN: 'https://api-pesantren-indonesia.vercel.app'
+  }
+};
+
+export default API_CONFIG;
